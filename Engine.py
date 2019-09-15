13379cca2e453fcf9f84e007890508137eba6166
Dhruv Patel <35431701+dhruvp97@users.noreply.github.com>
2019-09-15 00:29:42 -0400

    Added the article

--- Engine.py
+++ Engine.py
@@ -1,4 +1,5 @@
 import bin.News as News
+import bin.Article as Article
 import eel
 eel.init('web')
 
@@ -8,5 +9,10 @@ def classifyNewsArticle(title, article):
     article = ["" + str(article)]
     News.classifyNews(title, article)
 
+@eel.expose
+def classifyArticle(title, article):
+    title = ["" + str(title)]
+    article = ["" + str(article)]
+    Article.classifyArticle(title, article)
 
 eel.start('index.html', size=(1000,600))
---
+++ web/articles.js
@@ -0,0 +1,11 @@
+function classify() {
+    var title = document.getElementById("title").value;
+    var article = document.getElementById("article").value;
+    eel.classifyArticle(title, article);
+}
+
+function returnHome() {
+    setTimeout(function() {
+        window.open("index.html","_self");
+    }, 500);
+}
