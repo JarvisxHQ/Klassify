b792e95c7bc7cbdc20302f61f20b13b95ec31c71
Dhruv Patel <35431701+dhruvp97@users.noreply.github.com>
2019-09-14 23:18:20 -0400

    Added the loading spinners

--- web/main.js
+++ web/main.js
@@ -3,22 +3,18 @@ function printData() {
     var data = type.value;
     loadingSpinner = document.getElementById("spinner");
     if(data == "News") {
-        console.log("A")
         loadingSpinner.style = "color: #ffc107";
-    } else if(data == "Article") {
-        console.log("B");
+    } else if(data == "Articles") {
         loadingSpinner.style = "color: #28a745";
     } else if(data == "Tweets") {
-        console.log("C");
         loadingSpinner.style = "color: #007bff";
     } else if(data == "Emails") {
-        console.log("D");
         loadingSpinner.style = "color: #17a2b8";
     } else if(data == "Chats") {
-        console.log("E");
         loadingSpinner.style = "color: #f8f9fa";
     } else {
         type.placeholder = "Not valid"; 
+        type.value = "";
         loadingSpinner.style = "color: 	#000000";
     }
 }
