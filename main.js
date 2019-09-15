a88a3b182fa3acbc2a623a416f6e8bcf93150489
Dhruv Patel <35431701+dhruvp97@users.noreply.github.com>
2019-09-14 22:12:13 -0400

    Submit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const{app, BrowserWindow} = electron; 
+const{app, BrowserWindow, Menu} = electron; 
 let mainWindow; 
 
 app.on('ready', function() {
@@ -12,4 +12,40 @@ app.on('ready', function() {
         protocol:'file',
         slashes: true
     }));
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+    
 });
+
+function createNewArticleWindow() {
+    newsarticleWindow = new BrowserWindow({
+        width: 400,
+        height: 200,
+        title: 'News Article Analyzer'
+    });
+    newsarticleWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'newsarticle.html'),
+        protocol:'file',
+        slashes: true
+    }));
+}
+
+const mainMenuTemplate = [
+    {
+        label:'News Articles',
+        click(){
+            createNewArticleWindow(); 
+        }
+    }, {
+        label:'Articles'
+    }, {
+        label:'Tweets'
+    },  {
+        label:'Chats'
+    },  {
+        label:'Email'
+    },  {
+        label:'Help'
+    }
+];
