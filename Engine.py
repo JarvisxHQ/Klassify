5f3ea0316041d593ee84af88a580970dcd0a6aba
Dhruv Patel <35431701+dhruvp97@users.noreply.github.com>
2019-09-15 00:21:15 -0400

    Added the news

--- Engine.py
+++ Engine.py
@@ -1,4 +1,12 @@
+import bin.News as News
 import eel
 eel.init('web')
+
+@eel.expose
+def classifyNewsArticle(title, article):
+    title = ["" + str(title)]
+    article = ["" + str(article)]
+    News.classifyNews(title, article)
+
 
 eel.start('index.html', size=(1000,600))
