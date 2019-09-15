aeaec028a95ffbe705d9a12556dacc474b5980c8
Dhruv Patel <35431701+dhruvp97@users.noreply.github.com>
2019-09-15 00:03:25 -0400

    Added the html page for each type

--- web/main.js
+++ web/main.js
@@ -7,12 +7,16 @@ function printData() {
         classifyNews();
     } else if(data == "Articles") {
         loadingSpinner.style = "color: #28a745";
+        classifyArticle();
     } else if(data == "Tweets") {
         loadingSpinner.style = "color: #007bff";
+        classifyTweet();
     } else if(data == "Emails") {
         loadingSpinner.style = "color: #17a2b8";
+        classifyEmail();
     } else if(data == "Chats") {
         loadingSpinner.style = "color: #f8f9fa";
+        classifyChat();
     } else {
         type.placeholder = "Not valid"; 
         type.value = "";
@@ -23,5 +27,29 @@ function printData() {
 function classifyNews() {
     setTimeout(function() {
         window.open("news.html","_self");
+    }, 500);
+}
+
+function classifyArticle() {
+    setTimeout(function() {
+        window.open("articles.html","_self");
+    }, 500);
+}
+
+function classifyTweet() {
+    setTimeout(function() {
+        window.open("tweets.html","_self");
+    }, 500);
+}
+
+function classifyEmail() {
+    setTimeout(function() {
+        window.open("emails.html","_self");
+    }, 500);
+}
+
+function classifyChat() {
+    setTimeout(function() {
+        window.open("chats.html","_self");
     }, 500);
 }
